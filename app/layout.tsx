feaca41a5847abda405cedc319f964e39bea64e3
Mfitumukiza Peter <61537587+Peter-Mfitumukiza@users.noreply.github.com>
2023-09-12 11:59:32 +0200

    Update layout.tsx
    Removed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React from "react";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import "@radix-ui/themes/styles.css";
@@ -8,11 +7,6 @@ import { Theme } from "@radix-ui/themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Todo",
-  description: "Awesome app to track your tasks",
-};
 
 export default function RootLayout({
   children,
