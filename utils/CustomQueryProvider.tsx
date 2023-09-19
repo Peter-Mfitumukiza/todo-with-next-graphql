783eb6309aac2a962ad72cd4c4f2de6b8ec2ff0f
Peter-Mfitumukiza <mfitep6@gmail.com>
2023-09-19 15:12:27 +0200

    using env variables to create client

--- utils/CustomQueryProvider.tsx
+++ utils/CustomQueryProvider.tsx
@@ -14,7 +14,9 @@ export default function CustomQueryProvider({
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: process.env.NEXT_PUBLIC_VERCEL_URL
+            ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+            : "http://localhost:3000/api/trpc/",
         }),
       ],
     })
